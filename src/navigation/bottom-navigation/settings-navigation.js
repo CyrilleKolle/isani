4993bf4e411dd1ae4f51b24548e7c55bb4f24391
Cyrille <ngidekollecyrille@gmail.com>
2021-05-29 01:31:44 +0200

    Remove title in profile stack navigation

--- src/navigation/bottom-navigation/settings-navigation.js
+++ src/navigation/bottom-navigation/settings-navigation.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Settings from "../../scenes/account/settings";
-import SignIn from "../../components/user/signIn";
-import SignUp from "../../components/user/signUp";
 import Messages from "../../scenes/account/Messages";
 import ProfileInformation from "../../scenes/account/ProfileInformation";
-import { Button } from "react-native";
 import AuthScreenModal from "../../components/Modal/modalScenes/AuthModalScreen";
 import Home from "../../scenes/home/home";
 import TermsConditions from "../../scenes/account/termsConditions";
@@ -13,16 +10,20 @@ import AccountSettings from "../../scenes/account/AccountSettings";
 import FAQ from "../../scenes/account/FAQ";
 import CustomerService from "../../scenes/account/CustomerService";
 import Reviews from "../../scenes/account/Reviews";
+
 const Stack = createStackNavigator();
 
 export default function SettingsNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+      }}
+    >
       <Stack.Screen name="Settings Page" component={Settings} />
       <Stack.Screen name="Messages" component={Messages} />
       <Stack.Screen name="ProfileInformation" component={ProfileInformation} />
       <Stack.Screen name="Modal" component={AuthScreenModal} />
-      {/* <Stack.Screen name="Home" component={Home} /> */}
       <Stack.Screen name="Terms" component={TermsConditions} />
       <Stack.Screen name="AccountSettings" component={AccountSettings} />
       <Stack.Screen name="FAQ" component={FAQ} />
