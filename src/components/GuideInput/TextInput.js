f2e0639f80c4fc9c3a64582cfb06225dacf8dd2b
Cyrille <ngidekollecyrille@gmail.com>
2021-05-29 01:30:01 +0200

    Add text input

---
+++ src/components/GuideInput/TextInput.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Dimensions } from "react-native";
+import { Text } from "react-native-paper";
+
+const { width, height } = Dimensions.get("screen");
+
+export default function TextInput({ labelName, ...rest }) {
+  return (
+    <Text style={styles.input} numberOfLines={1} {...rest} >
+      {labelName}
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+
+
+    width: width / 1.5,
+    height: height / 15,
+  },
+});
